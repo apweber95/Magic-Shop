cded352df8082d0c452ca41d81fa411ba09ea147
Kerwin Chu <theguygalaxy@gmail.com>
2019-08-30 11:35:05 -0400

    Adding BackpackItem service and controller

--- MagicShop/src/main/java/com/revature/controller/BackpackItemController.java
+++ MagicShop/src/main/java/com/revature/controller/BackpackItemController.java
@@ -1,18 +1,27 @@
 package com.revature.controller;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.revature.service.BackpackItemService;
+import com.revature.beans.BackpackItem;
+import com.revature.data.BackpackItemDAO;
 
 @RestController
 @CrossOrigin(origins="http://localhost:4200")
 @RequestMapping(value="/backpack")
 public class BackpackItemController {
 	@Autowired
-	private BackpackItemService bs;
+	private BackpackItemDAO bd;
 	
-	
+	@GetMapping
+	public ResponseEntity<Set<BackpackItem>> getBackpackItems(@PathVariable Integer id){
+		return ResponseEntity.ok(bd.getBackpackItemsByOwnerID(id));
+	}
 }
