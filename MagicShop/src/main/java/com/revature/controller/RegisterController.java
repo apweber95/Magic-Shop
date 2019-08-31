18a21a54957b5bdd02afe7cd4317a79da16d56a3
JonHess4 <38571343+JonHess4@users.noreply.github.com>
2019-08-31 18:03:26 -0400

    added LoggingAspect

--- MagicShop/src/main/java/com/revature/controller/RegisterController.java
+++ MagicShop/src/main/java/com/revature/controller/RegisterController.java
@@ -1,8 +1,11 @@
 package com.revature.controller;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +16,7 @@ import com.revature.service.HumanService;
 
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins="http://localhost:4401")
 @RequestMapping(value="/register")
 
 public class RegisterController {
@@ -21,8 +24,14 @@ public class RegisterController {
 	@Autowired
 	private HumanService hs;
 	
+	@GetMapping
+	public ResponseEntity<Human> register() {
+		System.out.println("Here it is");
+		return ResponseEntity.ok(hs.getByID(1));
+	}
+	
 	@PostMapping
-	public ResponseEntity<Integer> register(@RequestBody Human human){
+	public ResponseEntity<Integer> register(@RequestBody Human human, HttpServletResponse resp){
 		System.out.println("Registering new Account: " + human);
 		return ResponseEntity.ok(hs.createHuman(human));
 	}
