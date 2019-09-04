a7aa2cc22b7c9231e6ad57818a8f667b404c4177
wthompson12 <wthomp31@uncc.edu>
2019-09-03 20:41:36 -0400

    Minor changes

--- magic/src/app/app-routing.module.ts
+++ magic/src/app/app-routing.module.ts
@@ -2,13 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './core/login/login.component'
 import {BackpackComponent} from './items/backpack/backpack.component';
-<<<<<<< HEAD
 import {CartComponent} from './items/cart/cart.component';
-
-
-=======
 import {ItemShelfComponent } from './items/item-shelf/item-shelf.component';
->>>>>>> 875e29cef519336f383b3ecfdc7fa22bd9c8d6b8
 
 const routes: Routes = [
   // {
