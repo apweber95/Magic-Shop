4a72ecde266854a9c248007da54a35dc043d4b02
Kerwin Chu <theguygalaxy@gmail.com>
2019-09-04 09:48:57 -0400

    Changing method in cart item controller

--- MagicShop/src/main/java/com/revature/controller/CartItemController.java
+++ MagicShop/src/main/java/com/revature/controller/CartItemController.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import com.revature.beans.CartItem;
 import com.revature.beans.Human;
@@ -22,9 +23,10 @@ public class CartItemController {
 	@Autowired
 	private CartItemDAO cid;
 	
-	@GetMapping
-	public ResponseEntity<CartItem> addCartItem(CartItem cartItem){
-		return ResponseEntity.ok(cid.addCartItem(cartItem));
+	@PostMapping
+	public ResponseEntity<CartItem> addCartItem(@RequestBody CartItem cartItem){
+		cid.addCartItem(cartItem);
+		return ResponseEntity.ok(cartItem);
 	}
 
 	@GetMapping
