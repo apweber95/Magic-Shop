87548bbe8531af3d8bc7c69bf7d17910f25b56c0
Kerwin Chu <theguygalaxy@gmail.com>
2019-09-04 21:52:47 -0400

    Add items to cart in progress

--- magic/src/app/items/item-shelf/item-shelf.component.ts
+++ magic/src/app/items/item-shelf/item-shelf.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { BackpackItem } from '../shared/backpack';
 import { BackpackService } from '../shared/backpack.service';
+import { CartService } from '../shared/cart.service';
+import { CartItem } from '../shared/cart';
+import { Human } from 'src/app/shared/human';
+import { LoginService } from 'src/app/shared/login.service';
 
 @Component({
   selector: 'app-item-shelf',
@@ -11,7 +15,14 @@ export class ItemShelfComponent implements OnInit {
   displayColumns: string[] = ['name', 'description', 'rarity', 'shelfPrice', 'stock']
   bItems: BackpackItem[];
   searchText: string;
-  constructor(private backpackService: BackpackService) { }
+  public cartItem: CartItem = new CartItem();
+  loggedHuman: Human;
+
+  constructor(
+    private backpackService: BackpackService,
+    private cartService: CartService,
+    private loginService: LoginService
+  ) { }
 
   ngOnInit() {
     this.backpackService.getBackpackItemsByOwnerID(1).subscribe( (bItems) => {
@@ -20,7 +31,19 @@ export class ItemShelfComponent implements OnInit {
     });
   }
 
-  addToCart(){
-    console.log("addToCart works");
+  addToCart(bp: BackpackItem){
+    this.loggedHuman = this.loginService.getHuman();
+    //TODO
+    // this.cartItem.cartItemID = 
+    this.cartItem.ownerID = this.loggedHuman;
+    this.cartItem.itemID = bp.itemID;
+    this.cartItem.amount = 1;
+    console.log(this.cartItem);
+  }
+
+  enableButton(){
+    if(this.loginService.getHuman()){
+      return true;
+    }
   }
 }
