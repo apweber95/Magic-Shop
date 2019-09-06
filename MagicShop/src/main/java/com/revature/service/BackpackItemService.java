4893cea91ecdd6f191475a6be7cfff1b5fd4d37f
JonHess4 <38571343+JonHess4@users.noreply.github.com>
2019-09-06 13:20:49 -0400

    fleshing out quest functions in adventure board

--- MagicShop/src/main/java/com/revature/service/BackpackItemService.java
+++ MagicShop/src/main/java/com/revature/service/BackpackItemService.java
@@ -54,6 +54,6 @@ public class BackpackItemService {
 				return backpackItem;
 			}
 		}
-		return new BackpackItem();
+		return null;
 	}
 }
--- magic/src/app/components/adventure-board/adventure-board.component.ts
+++ magic/src/app/components/adventure-board/adventure-board.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { SnackbarService} from '../../services/snackbar.service';
+import { LoginService } from '../../shared/login.service';
+import { Human } from '../../beans/human';
+import { QuestService } from '../../services/quest.service';
+import { UrlService } from '../../shared/url.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-adventure-board',
@@ -8,11 +15,24 @@ import { SnackbarService} from '../../services/snackbar.service';
 })
 export class AdventureBoardComponent implements OnInit {
 
+  url = this.urlService.getUrl();
+  private headers = new HttpHeaders({'Content-Type': 'application/json'});
+
   constructor(
-    private snackbarService: SnackbarService
+    private snackbarService: SnackbarService,
+    private loginService: LoginService,
+    private questService: QuestService, 
+    private urlService: UrlService,
+    private http: HttpClient
   ) { }
 
   ngOnInit() {
+  }
+
+  quest(rate: number): Observable<Human> {
+    let human: Human = this.loginService.getHuman();
+    const body = JSON.stringify(human);
+    return this.http.post(this.url+"humans/", body, {headers: this.headers, withCredentials: true}).pipe(map(resp => resp as Human));
   }
 
 }
