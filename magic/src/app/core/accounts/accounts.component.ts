50578a1ae5a8612ec8813202f8e2e40ea30dfe17
wthompson12 <wthomp31@uncc.edu>
2019-09-06 13:27:52 -0400

    Role Status

--- magic/src/app/core/accounts/accounts.component.ts
+++ magic/src/app/core/accounts/accounts.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Human } from 'src/app/shared/human';
 import { AccountsService } from 'src/app/shared/accounts.service';
+import { LoginService } from 'src/app/shared/login.service';
 
 @Component({
   selector: 'app-accounts',
@@ -9,10 +10,46 @@ import { AccountsService } from 'src/app/shared/accounts.service';
 })
 export class AccountsComponent implements OnInit {
   public accounts: Human[];
-  constructor(private accountsService: AccountsService) { }
+  selectedRow : Human;
+  setClickedRow : Function;
+  private human: Human;
+  public changingHuman: Human;
+  loggedHuman: Human;
+  constructor(private accountsService: AccountsService, private loginService: LoginService) { }
 
   ngOnInit() {
     this.accountsService.getAccounts().subscribe( accounts => this.accounts = accounts);
+    this.setClickedRow = function(index){
+      this.selectedRow = index;
+      console.log(this.loggedHuman.roleID);
+    console.log(this.selectedRow);
+    console.log(this.loggedHuman.roleID);
+    }
+
   }
+
+toFired(h: Human){
+  this.loggedHuman = this.loginService.getHuman();
+
+  if(this.setClickedRow && this.loggedHuman.roleID == 2){
+  this.changingHuman = this.selectedRow;
+  this.accountsService.updateHuman(this.changingHuman).subscribe( human => this.human = human);
+
+  h.roleID = h.roleID + 1;
+  }
+}
+toPromoted(h: Human){
+  this.loggedHuman = this.loginService.getHuman();
+  if(this.setClickedRow && this.loggedHuman.roleID == 3){
+  console.log(this.loggedHuman);
+  this.changingHuman = this.selectedRow;
+  this.accountsService.updateHuman(this.changingHuman).subscribe( human => this.human = human);
+
+  h.roleID = h.roleID - 1;
+  }
+ 
+
+}
+  
 
 }
